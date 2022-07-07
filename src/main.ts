95cdaa4ba144df4304f009ebc215cdbb7c153035
kadu <kaduzera.ks@gmail.com>
2022-07-07 02:50:38 -0300

    install swaggerUI

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Using NestJS')
+    .setDescription('Blog API description')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
