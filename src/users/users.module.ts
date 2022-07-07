547d7abfa6e46d3c69a53bdf84303536ef73fb08
kadu <kaduzera.ks@gmail.com>
2022-07-06 23:23:21 -0300

    user service implemented

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,3 +1,4 @@
+import { UsersRepository } from './repositories/user.repository';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
@@ -5,6 +6,6 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, UsersRepository],
 })
 export class UsersModule {}
